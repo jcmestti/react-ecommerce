fa76d6cffdfa95f515d5c84e0a2d626571d3c67f
Joao Carlos Mestti <jcmestti@limaconsulting.com>
2023-03-20 07:47:12 -0300

    firebase app/auth/firestore addedd

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,26 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import {
+    auth,
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+    signInWithGoogleRedirect
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+    
+    useEffect(() => {
+        (
+            async () => {
+                const response = await getRedirectResult(auth);
+                if (response) {
+                    const userDocRef = await createUserDocumentFromAuth(response.user);
+                }
+            }
+        )()
+    }, []);
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
@@ -10,6 +30,7 @@ const SignIn = () => {
         <div>
             <h1> Sign In page</h1>
             <button onClick={logGoogleUser}> Sign in with Google popup</button>
+            <button onClick={signInWithGoogleRedirect}> Sign in with Google Redirect</button>
         </div>
     )
 }
