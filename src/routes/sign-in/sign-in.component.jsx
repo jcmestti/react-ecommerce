5fbb3dd6b8186a56af2b366574f714a600d717df
João Carlos Mestti <jcmestti@gmail.com>
2023-03-20 11:35:04 -0300

    buidilng sign up form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { getRedirectResult } from 'firebase/auth';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 import {
     auth,
@@ -10,7 +11,7 @@ import {
 
 const SignIn = () => {
     
-    useEffect(() => {
+    /* useEffect(() => {
         (
             async () => {
                 const response = await getRedirectResult(auth);
@@ -19,7 +20,7 @@ const SignIn = () => {
                 }
             }
         )()
-    }, []);
+    }, []); */
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -30,7 +31,8 @@ const SignIn = () => {
         <div>
             <h1> Sign In page</h1>
             <button onClick={logGoogleUser}> Sign in with Google popup</button>
-            <button onClick={signInWithGoogleRedirect}> Sign in with Google Redirect</button>
+            <SignUpForm />
+            {/* <button onClick={signInWithGoogleRedirect}> Sign in with Google Redirect</button> */}
         </div>
     )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -29,17 +29,20 @@ googleProvider.setCustomParameters({
 });
 
 export const auth = getAuth();
-console.log(auth);
+
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
-export const db = getFirestore();
+export const db = getFirestore(firebaseApp);
 
 export const createUserDocumentFromAuth = async (userAuth) => {
+
     const userDocRef =  doc(db, 'users', userAuth.uid);
-
+    console.log('userDocRef:');
+    console.log(userDocRef);
     const userSnapshot = await getDoc(userDocRef);
-
+    console.log('userSnapshot:');
+    console.log(userSnapshot);
     if(!userSnapshot.exists()) {
         const { displayName, email } = userAuth;
         const createdAt = new Date();
